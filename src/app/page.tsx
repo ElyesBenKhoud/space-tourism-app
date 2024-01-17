1956780e2370fd430f51e05a5b6e353454ca251c
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2024-01-17 15:42:09 +0200

    home page bg set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+"use client";
+
+import Background from "@/components/Background";
+import React from "react";
+import mobile from "@/assets/home/background-home-mobile.jpg";
+import tablet from "@/assets/home/background-home-tablet.jpg";
+import desktop from "@/assets/home/background-home-desktop.jpg";
 
 export default function Home() {
-  return <p></p>;
+  return (
+    <>
+      <Background
+        mobile={mobile.src}
+        tablet={tablet.src}
+        desktop={desktop.src}
+      />
+    </>
+  );
 }
