f8972df86941ac9917186a1c7f438b978b3f2426
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2024-01-18 12:16:06 +0200

    routing pages settled

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -4,8 +4,8 @@ function page() {
   return (
     <main className=" relative z-10 p-6 md:px-24 md:pb-0 ">
       <div className="w-fit nav-text uppercase text-white md:tracking-[3px] md:text-[20px]  lg:text-[28px] lg:tracking-[5px]">
-        <span className="inline-block font-bold opacity-25 mr-5">02</span>
-        <span className="inline-block">Meet your crew</span>
+        <span className="inline-block font-bold opacity-25 mr-5">03</span>
+        <span className="inline-block">Space Launch 101</span>
       </div>
     </main>
   );
