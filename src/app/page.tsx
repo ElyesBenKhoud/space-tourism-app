9467d383048600fa568060ffbb7e0a70f1d69bda
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2024-01-18 09:00:47 +0200

    classname refactored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         desktop={desktop.src}
       />
 
-      <main className="relative z-10 min-h-[80vh] flex flex-col justify-evenly items-center lg:flex-row lg:items-end lg:justify-between lg:p-32 ">
+      <main className="flex flex-col justify-evenly items-center relative z-10 min-h-[80vh] lg:flex-row lg:items-end lg:justify-between lg:p-32 ">
         <div className="font-primary text-secondary text-center px-4 md:max-w-lg md:mx-auto lg:mx-0 lg:text-left">
           <span className="nav-text text-lg uppercase lg:-translate-x-8 lg:text-3xl">
             {" "}
@@ -33,7 +33,7 @@ export default function Home() {
           onClick={() => {
             router.push("/destination");
           }}
-          className="bg-white w-48 h-48 rounded-full flex items-center justify-center font-primary tracking-[1.25px] uppercase relative group md:w-60 md:h-60 lg:w-72 lg:h-72"
+          className="flex items-center justify-center font-primary bg-white w-48 h-48 rounded-full tracking-[1.25px] uppercase relative group md:w-60 md:h-60 lg:w-72 lg:h-72"
         >
           <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[150%] h-[150%] bg-white/10 rounded-full scale-50 transition ease-in-out duration-[700ms!important] cursor-pointer group-hover:scale-100"></div>
           <span className="relative hover:cursor-pointer z-10 text-primary text-lg md:text-3xl">
