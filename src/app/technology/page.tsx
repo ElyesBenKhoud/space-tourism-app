b5fc3116ec08dff611487fd64b3e4bf7b4ae51ce
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2024-01-19 10:11:31 +0200

    technology route - done

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -1,13 +1,68 @@
-import React from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+import React, { useMemo, useState } from "react";
+import Image from "next/image";
+
+import Background from "@/components/Background";
+import PageTitle from "@/components/PageTitle";
+import { technologies } from "./Technologies";
+
+import mobile from "@/assets/technology/background-technology-mobile.jpg";
+import tablet from "@/assets/technology/background-technology-tablet.jpg";
+import desktop from "@/assets/technology/background-technology-desktop.jpg";
+import TechnologyContent from "@/components/TechnologyContent";
 
 function page() {
+  let [actualTechnology, setActualTechnology] = useState(0);
+
+  const technology = useMemo(
+    () => technologies[actualTechnology],
+    [actualTechnology]
+  );
+
   return (
-    <main className=" relative z-10 p-6 md:px-24 md:pb-0 ">
-      <div className="w-fit nav-text uppercase text-white md:tracking-[3px] md:text-[20px]  lg:text-[28px] lg:tracking-[5px]">
-        <span className="inline-block font-bold opacity-25 mr-5">03</span>
-        <span className="inline-block">Space Launch 101</span>
-      </div>
-    </main>
+    <div>
+      <Background
+        mobile={mobile.src}
+        tablet={tablet.src}
+        desktop={desktop.src}
+      />
+      <main className="relative z-10 w-full mt-10 lg:pl-44">
+        <div className="w-fit mx-auto mb-8 md:mx-2">
+          <PageTitle number={3} title="Space Launch 101" />
+        </div>
+        <div className="flex flex-col lg:flex-row-reverse">
+          <div className="w-full lg:w-[35%] lg:min-h-[35vh] ">
+            <source
+              media="(min-width:1024px)"
+              srcSet={technology.portrait.src}
+            />
+            <Image
+              src={technology.landscape}
+              className="object-cover w-full h-auto"
+              alt="Background Image"
+            />
+          </div>
+          <div className="flex flex-col items-center mt-8 md:mt-14 lg:flex-row lg:grow lg:mt-0">
+            <div className="w-fit flex gap-4 mb-6 md:mb-11 lg:flex-col lg:mb-0 lg:gap-8">
+              {technologies.map(({ name }, index) => (
+                <div
+                  onClick={() => setActualTechnology(index)}
+                  key={name}
+                  className={`w-10 md:w-14 aspect-square flex items-center justify-center text-white border rounded-full border-white/20 transition cursor-pointer
+                                    [&.active]:border-transparent [&.active]:bg-white [&.active]:text-primary [&:not(.active)]:hover:border-white ${
+                                      technology.name === name && "active"
+                                    }`}
+                >
+                  {index + 1}
+                </div>
+              ))}
+            </div>
+            <TechnologyContent technology={technology} />
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
 
